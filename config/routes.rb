f8e08183a34cea82b4b7590a04a5c326e262880f
kichinaga <pixo.3303@gmail.com>
2017-09-21 10:59:39 +0900

    stockコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :industries, only: [:index]
   resources :markets, only: [:index]
   resources :users
+  resources :stocks, only: [:show, :new, :create, :edit, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
