1d51659b8c01cb158c6a5eda27b27dbbbfc80987
kichinaga <pixo.3303@gmail.com>
2017-10-18 23:23:39 +0900

    add News Scraping Class
    日経新聞のサイトから企業の関連情報を入手

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,11 @@
 module CompaniesHelper
+  BASE_URL = 'https://www.nikkei.com/search/site/'
+
+  def get_news_list(stock_code)
+    Scraping::NewsScrape.set_capybara(BASE_URL)
+
+    scraper = Scraping::NewsScrape.new(stock_code)
+
+    scraper.get_news_link
+  end
 end
---
+++ lib/scraping/news_scrape.rb
@@ -0,0 +1,57 @@
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+
+module Scraping
+  class NewsScrape
+    include Capybara::DSL
+    CHARSET = 'utf-8'
+
+    def initialize(stock_code)
+      @url = URI.escape("https://www.nikkei.com/search/site/?searchKeyword=#{stock_code}")
+      @stock_code = stock_code
+      @articles = Array.new
+      open_page
+    end
+
+    def get_news_link
+      wait_for_ajax
+      find('app-result').all('article').each do |article|
+        link = article.find('a')
+        @articles.push({stock_code: @stock_code,
+                        title: link.find('h3').text,
+                        url: link[:href],
+                        date: Time.parse(link.find('.status').text[/（(.*?)）/, 1])
+                       })
+      end
+      @articles
+    end
+
+    def self.set_capybara(url)
+      Capybara.configure do |config|
+        config.run_server = false
+        # config.current_driver = :poltergeist
+        config.javascript_driver = :poltergeist
+        config.ignore_hidden_elements = true
+        config.app_host = url
+        config.default_max_wait_time = 5
+      end
+
+      Capybara.register_driver :poltergeist do |app|
+        Capybara::Poltergeist::Driver.new(
+            app, { timeout: 120, js_errors: false, phantomjs_options: %w(--load-images=no --ignore-ssl-errors=no --ssl-protocol=any) }
+        )
+      end
+    end
+
+    private
+      def wait_for_ajax
+        until find('app-result')
+        end
+      end
+
+      def open_page
+        visit @url
+      end
+  end
+end
