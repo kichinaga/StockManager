dd490a8cf07f1bdc2c3410d42f6e6a7e1f5f215c
kichinaga <pixo.3303@gmail.com>
2017-10-18 23:21:52 +0900

    fix class name
    クラス名の変更

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -2,9 +2,9 @@ module StocksHelper
   BASE_URL = 'http://quote.jpx.co.jp/jpx/template/quote.cgi?F=tmp/stock_search'
 
   def get_now_price(stock_code)
-    Scraping::StocksScrape.set_capybara(BASE_URL)
+    Scraping::JPXScrape.set_capybara(BASE_URL)
 
-    scraper = Scraping::StocksScrape.new(stock_code)
+    scraper = Scraping::JPXScrape.new(stock_code)
 
     scraper.get_now_price
   end
