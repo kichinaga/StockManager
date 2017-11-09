bfe6c5fbf4062a2cfd238a8a67c078d85cffacf1
kichinaga <pixo.3303@gmail.com>
2017-11-09 14:47:48 +0900

    [fix] news scraping program
    日経新聞のサイトのデザインが変わったので、新しいデザインのサイトにも対応させました

--- lib/scraping/news_scrape.rb
+++ lib/scraping/news_scrape.rb
@@ -8,12 +8,14 @@ module Scraping
     CHARSET = 'utf-8'
 
     def initialize(stock_code)
-      @url = "https://www.nikkei.com/search/site/?searchKeyword=#{stock_code}"
+      # @url = "https://www.nikkei.com/search/site/?searchKeyword=#{stock_code}"
+      @url = "https://www.nikkei.com/nkd/company/news/?scode=#{stock_code}"
       @stock_code = stock_code
       @articles = Array.new
       open_page
     end
 
+=begin
     def get_news_link
       wait_for_ajax
       find('app-result').all('article').each do |article|
@@ -26,7 +28,23 @@ module Scraping
       end
       @articles
     end
+=end
 
+    def get_news_link
+      doc = Nokogiri::HTML.parse(html)
+      doc.css('div.m-listFormat').each do |div|
+        div.css('li').each do |item|
+          @articles.push({
+                             stock_code: @stock_code,
+                             title: item.css('a').inner_text,
+                             url: "https://www.nikkei.com#{item.css('a')[0][:href]}",
+                             date: item.css('span.m-listItem_time').inner_text
+                         })
+        end
+      end
+
+      @articles
+    end
     def self.set_capybara(url)
       Capybara.configure do |config|
         config.run_server = false
@@ -52,6 +70,7 @@ module Scraping
 
       def open_page
         visit @url
+        # find('div.pageNav_body').click_link('ニュース')
       end
   end
 end
